324c7e0e598ab49e94e3e77604293c69084abd57
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-10-21 16:16:59 +0530

    create component functioning added

--- src/app/blog-create/blog-create.component.ts
+++ src/app/blog-create/blog-create.component.ts
@@ -12,8 +12,8 @@ export class BlogCreateComponent implements OnInit {
   constructor(private blogHttpService:BlogHttpService) { }
 
   //always intitialize the values at the beginning
-
-  public blogTitle:string;
+//the tow way binding can be seen here,as we give value to blogTitle it is reflected in the view component ,and if the value is changed there it will be changed here too
+  public blogTitle:string="this is a default title";
   public blogDescription:string;
   public blogBodyHtml:string;
   public blogCategory:string;
@@ -33,8 +33,8 @@ export class BlogCreateComponent implements OnInit {
 
     //calling the service and alos processing the response
     this.blogHttpService.createBlog(blogData).subscribe(
-      data=>{console.log(data);
-            alert(data);
+      data=>{console.log(data.message);
+            //alert(data);
             alert("Blog Posted Succesfully");},
       error=>{console.log("error in creating blog");
               console.log(error.message);});
